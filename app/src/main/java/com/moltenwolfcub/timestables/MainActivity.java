feed1cde7d880d47c1c7cf52e723ec5a5b7176e9
MoltenWolfCub <oliver@tomlins.org.uk>
2026-06-09 16:40:09 +0100

    Got basic activity working

--- app/src/main/java/com/moltenwolfcub/timestables/MainActivity.java
+++ app/src/main/java/com/moltenwolfcub/timestables/MainActivity.java
@@ -1,18 +1,33 @@
 package com.moltenwolfcub.timestables;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText maxTable;
+
+    private int maxTableValue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.home);
+        setContentView(R.layout.activity_main);
+
+        maxTableValue = 1;
+
+        maxTable = findViewById(R.id.editTextNumber);
+
+        maxTable.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_DONE || (event != null && event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN)) {
+                maxTableValue = Integer.parseInt(maxTable.getText().toString());
+                return true;
+            }
+            return false;
+        });
     }
 }
